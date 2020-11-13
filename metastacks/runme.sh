12f09a549163915cb6f3244b03a2714fee117df2
Chip Lynch <chip@chiplynch.com>
2020-11-12 21:27:42 -0800

    metastacks endurance changes

--- metastacks/runme.sh
+++ metastacks/runme.sh
@@ -1,3 +1,9 @@
+mkdir -p ./endurance/
+mkdir -p ./endurance/gitlab/config
+mkdir -p ./endurance/gitlab/data
+mkdir -p ./endurance/gitlab/logs
+mkdir -p ./endurance/jenkins/
+mkdir -p ./endurance/postgres/
 docker-compose -f metastacks.yml up -d --remove-orphans
 
 # to connect use:
